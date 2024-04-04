2f3322290cd6f71947f3d573a02f6f8d8278d173
mobahaa2001.Mego <mobahaa2001.mobahaa2001@gmail.com>
2024-04-04 05:26:05 +0200

    Navigation and rendering the bottom tab

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from 'react';
+import BottomTabNavigation from './navigation/BottomTabNavigation';
+import { Cart } from './screens';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,22 +33,20 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyles}>New </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Bottom Navigation'
+          component={BottomTabNavigation}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name='Cart'
+          component={Cart}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyles: {
-    fontFamily: "regular",
-    fontSize: 20,
-  }
-});
