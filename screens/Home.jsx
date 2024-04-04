687c08b98279f85d2f80d89961f04727a5830d3c
mobahaa2001.Mego <mobahaa2001.mobahaa2001@gmail.com>
2024-04-04 05:58:07 +0200

    Home page and head bars

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {Ionicons, Fontisto} from "@expo/vector-icons";
+import styles from './home.style'
+
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+            <Ionicons name='location-outline' size={24} />
+
+            <Text style={styles.location}>Cairo, Egypt</Text>
+
+            <View style={{alignItems: "flex-end"}}>
+              <View style={styles.cartCount}>
+                  <Text style={styles.cartNumber}> 8 </Text>
+              </View>
+              <TouchableOpacity>
+                <Fontisto name='shopping-bag' size={24}/>
+              </TouchableOpacity>
+            </View>
+
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
