80f882b7d2055e9098b0f67463d44c991dca19f3
mobahaa2001.Mego <mobahaa2001.mobahaa2001@gmail.com>
2024-04-04 04:45:33 +0200

    Loading custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>New App</Text>
+      <Text style={styles.textStyles}>New </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +42,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyles: {
+    fontFamily: "regular",
+    fontSize: 20,
+  }
 });
