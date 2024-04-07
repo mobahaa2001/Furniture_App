d09748a7226c9972d012b1a3543a1d58b7c851af
mobahaa2001.Mego <mobahaa2001.mobahaa2001@gmail.com>
2024-04-07 04:57:01 +0200

    Home page and carousel

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import {Ionicons, Fontisto} from "@expo/vector-icons";
 import styles from './home.style'
 import { Welcome } from '../components';
+import Carousel from '../components/home/Carousel';
 
 
 const Home = () => {
@@ -27,6 +28,7 @@ const Home = () => {
       </View>
       <ScrollView>
         <Welcome />
+        <Carousel />
       </ScrollView>
     </SafeAreaView>
   )
